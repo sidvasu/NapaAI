3570fbc3da53bc257fb1f9253cb5392b7ba50f5c
Siddharth Vasu <siddharth.vasu.ca@gmail.com>
2025-05-03 14:55:39 -0700

    Saved model

--- model.py
+++ model.py
@@ -67,6 +67,7 @@ def recommend(item_id, num):
     for rec in recs:
         print('Recommended: ' + item(rec[1]) + ' (score: ' + f"{rec[0]:.2f}" + ')')
 
+'''
 def save_model(tfidf_vectorizer, svd_model, tfidf_reduced, train_wine_df, results_dict, folder='wine_model'):
     os.makedirs(folder, exist_ok=True)
     
@@ -78,7 +79,7 @@ def save_model(tfidf_vectorizer, svd_model, tfidf_reduced, train_wine_df, result
     
     print(f"Model saved to folder: {folder}")
 
-def load_model(folder='wine_model'):
+def load_model(folder):
     tfidf_vectorizer = joblib.load(f'{folder}/tfidf_vectorizer.joblib')
     svd_model = joblib.load(f'{folder}/svd_model.joblib')
     tfidf_reduced = np.load(f'{folder}/tfidf_reduced.npy')
@@ -90,6 +91,8 @@ def load_model(folder='wine_model'):
 
 # Saving
 save_model(tf, svd, tfidf_reduced, train_wine, results)
+tf, svd, tfidf_reduced, train_wine, results = load_model('wine_model')
+'''
 
 # Run a test
 itemId_ = train_wine['wineId'].values[0]
@@ -103,56 +106,3 @@ description_original = train_wine.loc[train_wine['title'] == itemName_, 'descrip
 description_matched = train_wine.loc[train_wine['wineId'] == results[itemId_][0][1], 'description'].values[0]
 
 print(f"\nOriginal Wine Description:\n{description_original}\n\nMatched Wine Description:\n{description_matched}")
-
-'''
-wine_df.loc[:, 'wineId'] = wine_df.loc[:, 'title'].astype('category').cat.codes
-wine_df.loc[:, ['description', 'wineId', 'title']].head()
-
-train_wine, test_wine = train_test_split(wine_df, train_size=0.05)
-
-train_wine.reset_index(drop=True, inplace=True)
-
-print(f"Training on {len(train_wine)} samples.")
-
-tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df = 1, stop_words='english')
-
-tfidf_matrix = tf.fit_transform(train_wine['description'])
-
-print(f"The term-frequency inverse document frequency matrix is {tfidf_matrix.shape[0]} by {tfidf_matrix.shape[1]}")
-
-cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
-
-# Next, appending the results to a dictionary of the similar items to each wine
-results = {}
-for idx, row in train_wine.iterrows():
-    similar_indices = cosine_similarities[idx].argsort()[:100:-1]
-    similar_items = [(cosine_similarities[idx][i], train_wine['wineId'][i]) for i in similar_indices]
-    results[row['wineId']] = similar_items[1:]
-
-def item(id):
-    return train_wine.loc[train_wine['wineId'] == id]['title'].tolist()[0].split(' - ')[0]
-
-def recommend(item_id, num):
-    print('Recommending ' + str(num) + ' products similar to ' + item(item_id) + ' ...')
-    print('-----')
-    recs = results[item_id][:num]
-    for rec in recs:
-        print('Recommended: ' + item(rec[1]) + '(score: ' + f"{rec[0]:.2f}" + ')')
-
-itemId_ = train_wine.loc[:, 'wineId'].values[0]
-itemName_ = train_wine.loc[train_wine['wineId'] == itemId_, 'title'].values[0]
-
-print(f"Using itemId {itemId_} which is {itemName_} \n")
-
-# The recommend function is then run to find and return the top num matches (5 in this case)
-
-recommend(item_id=itemId_, num=5)
-
-results[itemId_][0][1]
-
-description_original = train_wine.loc[train_wine['title'] == itemName_, 'description'].values[0]
-
-description_matched = train_wine.loc[train_wine['wineId'] == results[itemId_][0][1], 'description'].values[0]
-
-print(f"First wine description: \n{description_original} \n\nMatched wine description: \n{description_matched}")
-'''
